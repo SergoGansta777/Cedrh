512ed2823f03ce3e5446ec7e022c5de6dfe84752
SergoGansta777 <sergo777ser777@gmail.com>
2023-11-02 22:10:11 +0300

    Added vim welcome window like showed in tutorial

---
+++ notes.md
@@ -0,0 +1,3 @@
+# Some notes while code this editor
+
+In the report will be interesting to tell about VT100 and escape secuences
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,10 @@
-use std::io::{self, stdout, Read};
-use termion::raw::IntoRawMode;
+mod editor;
+mod terminal;
 
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
-}
+use editor::Editor;
+pub use terminal::Terminal;
+pub use editor::Position;
 
-fn die(e: std::io::Error) {
-    panic!("{}", e);
-}
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-
-                let c = b as char;
-
-                if c.is_control() {
-                    println!("{:?} \r", b);
-                } else {
-                    println!("{:?} ({})\r", b, c);
-                }
-
-                if b == to_ctrl_byte('q') {
-                    break;
-                }
-            }
-            Err(err) => die(err),
-        }
-    }
+    Editor::default().run();
 }
