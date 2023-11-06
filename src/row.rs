4cc4a8465eeeda9c0fa21c3e425b085570583808
SergoGansta777 <sergo777ser777@gmail.com>
2023-11-06 18:59:45 +0300

    Complete a prototype of editor

--- src/row.rs
+++ src/row.rs
@@ -10,6 +10,7 @@ use unicode_segmentation::UnicodeSegmentation;
 pub struct Row {
     string: String,
     highlighting: Vec<highlighting::Type>,
+    pub is_highlighted: bool,
     len: usize,
 }
 
@@ -18,6 +19,7 @@ impl From<&str> for Row {
         Self {
             string: String::from(slice),
             highlighting: Vec::new(),
+            is_highlighted: false,
             len: slice.graphemes(true).count(),
         }
     }
@@ -124,9 +126,11 @@ impl Row {
 
         self.string = row;
         self.len = length;
+        self.is_highlighted = false;
         Self {
             string: splitted_row,
             highlighting: Vec::new(),
+            is_highlighted: false,
             len: splitted_length,
         }
     }
@@ -420,8 +424,19 @@ impl Row {
         word: &Option<String>,
         start_with_comment: bool,
     ) -> bool {
-        self.highlighting = Vec::new();
         let chars: Vec<char> = self.string.chars().collect();
+        if self.is_highlighted && word.is_none() {
+            if let Some(hl_type) = self.highlighting.last() {
+                if *hl_type == highlighting::Type::MultilineComment
+                    && self.string.len() > 1
+                    && self.string[self.string.len() - 2..] == *"*/"
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        self.highlighting = Vec::new();
         let mut index = 0;
         let mut in_ml_comment = start_with_comment;
         if in_ml_comment {
@@ -457,6 +472,7 @@ impl Row {
         if in_ml_comment && &self.string[self.string.len().saturating_sub(2)..] != "*/" {
             return true;
         }
+        self.is_highlighted = true;
         false
     }
 }
