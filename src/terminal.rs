d7cffef12f556066ecf0f8eae7e946304cbebcb6
SergoGansta777 <sergo777ser777@gmail.com>
2023-12-31 09:53:40 +0300

    Optimizing output to get rid of flickering effect

--- src/terminal.rs
+++ src/terminal.rs
@@ -4,7 +4,7 @@ use crossterm::{
     cursor,
     event::{read, Event},
     style::{Color, ResetColor, SetBackgroundColor, SetForegroundColor},
-    terminal, ExecutableCommand,
+    terminal, QueueableCommand,
 };
 use std::io::{self, Error, Stdout, Write};
 
@@ -41,15 +41,13 @@ impl Terminal {
 
     pub fn clear_screen(&mut self) {
         self.stdout
-            .execute(terminal::Clear(terminal::ClearType::All))
+            .queue(terminal::Clear(terminal::ClearType::All))
             .ok();
     }
 
     pub fn quit(&mut self) {
         self.reset_colors();
-        self.stdout
-            .execute(terminal::Clear(terminal::ClearType::All))
-            .ok();
+        self.clear_screen();
         println!("Goodbye!\r");
         terminal::disable_raw_mode().ok();
     }
@@ -63,7 +61,7 @@ impl Terminal {
         let y = y as u16;
 
         self.stdout
-            .execute(cursor::MoveTo(x.saturating_sub(1), y.saturating_sub(1)))
+            .queue(cursor::MoveTo(x.saturating_sub(1), y.saturating_sub(1)))
             .ok();
     }
 
@@ -76,36 +74,36 @@ impl Terminal {
     }
 
     pub fn cursor_hide(&mut self) {
-        self.stdout.execute(cursor::Hide).ok();
+        self.stdout.queue(cursor::Hide).ok();
     }
 
     pub fn cursor_show(&mut self) {
-        self.stdout.execute(cursor::Show).ok();
+        self.stdout.queue(cursor::Show).ok();
     }
 
     pub fn clear_current_line(&mut self) {
         self.stdout
-            .execute(terminal::Clear(terminal::ClearType::CurrentLine))
+            .queue(terminal::Clear(terminal::ClearType::CurrentLine))
             .ok();
     }
 
     pub fn reset_colors(&mut self) {
-        self.stdout.execute(ResetColor).ok();
+        self.stdout.queue(ResetColor).ok();
     }
 
     pub fn set_bg_color(&mut self, color: Color) {
-        self.stdout.execute(SetBackgroundColor(color)).ok();
+        self.stdout.queue(SetBackgroundColor(color)).ok();
     }
 
     pub fn reset_bg_color(&mut self) {
-        self.stdout.execute(SetBackgroundColor(Color::Reset)).ok();
+        self.stdout.queue(SetBackgroundColor(Color::Reset)).ok();
     }
 
     pub fn set_fg_color(&mut self, color: Color) {
-        self.stdout.execute(SetForegroundColor(color)).ok();
+        self.stdout.queue(SetForegroundColor(color)).ok();
     }
 
     pub fn reset_fg_color(&mut self) {
-        self.stdout.execute(SetForegroundColor(Color::Reset)).ok();
+        self.stdout.queue(SetForegroundColor(Color::Reset)).ok();
     }
 }
